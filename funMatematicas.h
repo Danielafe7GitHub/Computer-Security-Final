8c169fbc1be4973ab099991194455b0c666f9288
DanielaFe7GitHub <daniela.milon@ucsp.edu.pe>
2017-06-11 17:19:14 -0500

    Funciones Matematicas

---
+++ funMatematicas.h
@@ -0,0 +1,48 @@
+#include <NTL/ZZ.h>
+#include <iostream>
+
+using namespace std;
+using namespace NTL;
+
+
+ZZ generadorPrimos()
+{
+
+}
+
+ZZ generadorAleatorios()
+{
+
+}
+
+ZZ exponenciacionModular()
+{
+
+}
+
+void factorizacion()
+{
+
+}
+
+
+ZZ MCD()
+{
+
+}
+
+ZZ phi()
+{
+
+}
+
+void swap()
+{
+
+}
+
+void cambioBase()
+{
+
+}
+
