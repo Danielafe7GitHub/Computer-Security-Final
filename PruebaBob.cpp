5193382d4da2b136316e826eecd24e605f1561d6
JRod166 <jose.vilca.vargas@gmail.com>
2017-06-25 18:34:09 -0500

    Diffie hellman?

---
+++ PruebaBob.cpp
@@ -0,0 +1,14 @@
+//
+// Created by pokelover on 25/06/17.
+//
+
+#include "BOBServer.h"
+
+int main()
+{
+    readhelloClient();
+    cin.get();
+    serverHello();
+    cin.get();
+    intercambioClaves();
+}
