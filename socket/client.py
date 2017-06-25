eac90fd8c7a1c8442298717cfec097a5f64cba1c
JcCcari <jhoel.ccari@ucsp.edu.pe>
2017-06-25 17:27:13 -0500

    improving more and more sockets

--- socket/client.py
+++ socket/client.py
@@ -3,13 +3,13 @@ from time import sleep
 
 class CClient:
     port = 8888
-    address = "localhost"
     filePath = ""
     client = None
     file = None
     def __init__(self, address, port = 8888):
         self.address = address
         self.port = port
+        print ("conectando a: ", self.address)
         self.initClient(self.address, self.port)
 
     def initClient(self, address, port):
@@ -20,22 +20,24 @@ class CClient:
         # La siguiente linea intenta establecer la conexion con el servidor localhost, es decir con tu mismo ordenador
         # Si se quiere hacer una conexion con otro ordenador -> cambiar @Param "localhost" por la ip con la que desea conectarse
         #client.connect(("192.168.1.107", 8888))
-        self.client.connect(("localhost", port))
+        self.client.connect((self.address, port))
         #self.client.setblocking(0)
 
     def send(self, file):
         self.filePath = file
         msg = self.readFile(self.filePath)
         print (msg)
-        for piece in msg:
-            piece = self.removeTrash(piece)
+        for i in range(len(msg)):
+            piece = msg[i]
+            if ( i != len(msg)-1):
+                piece = self.removeTrash(piece)
             byte = piece.encode('utf-8')   # codificamos el mensaje en utf-8
             print (piece)
             sleep(0.000000005)
             #print ("sending ...", piece, byte)
             self.client.send(byte)  # Enviamos el mensaje codificado
-
         self.client.close()                  # Cerramos socket cliente
+
     def readFile(self, file):
         with open(file, "r") as file:  # open the file
             contents = file.readlines()  # put the lines to a variable (list).
