dca2f308a5eea8cc014bc4d632a8b0638dbe8775
JRod166 <jose.vilca.vargas@gmail.com>
2017-06-25 12:37:49 -0500

    Funciones del RC4

--- socket/client.py
+++ socket/client.py
@@ -6,8 +6,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #> Intentamos establecer la coneccion con el server en el puerto 8888
 # La siguiente linea intenta establecer la conexion con el servidor localhost, es decir con tu mismo ordenador
 # Si se quiere hacer una conexion con otro ordenador -> cambiar @Param "localhost" por la ip con la que desea conectarse
-client.connect(("localhost", 8888))
-
+client.connect(("192.168.1.102", 9000))
+#client.connect(("localhost", 8888))
 while True:
     msg = str(input("> "))      # Ingresamos un mensaje
     byte = msg.encode('utf-8')  # codificamos el mensaje en utf-8
--- socket/server.py
+++ socket/server.py
@@ -8,7 +8,7 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #> Asociamos a un host publico en el puerto 8888
 #NOTA: el cliente y el servidor tienen que establecer conexion por el mismo puerto
 server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-server.bind(('', 8888))
+server.bind(('', 9000))
 
 #> listen le dice al servidor que tiene que escuchar 5 veces
 server.listen(5)
