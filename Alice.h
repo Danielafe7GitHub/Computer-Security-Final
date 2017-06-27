c83841a7fac7ebf47dcde020ea3d5db255fe3ac0
JRod166 <jose.vilca.vargas@gmail.com>
2017-06-27 16:56:17 -0500

    algoritmos compartidos

--- Alice.h
+++ Alice.h
@@ -1,7 +1,10 @@
+#ifndef Alice
+#define Alice
+
 #include <iostream>
 #include <fstream>
 #include "diffieHellman.h"
-#include "cesar.h"
+
 using namespace std;
 ZZ masterKey;
 
@@ -49,28 +52,5 @@ void intercambioClaves()
 }
 
 
-void writecifradoServer()
-{
-    /*A partir de este punto todo será cifrado*/
-    Cesar cesarin (masterKey);
-    string textoCifrado = cesarin.cipher("A partir de este punto todo seá cifrado. Atte: Alice");
-    ofstream clientHello;
-    clientHello.open("documento.txt" , std::ofstream::trunc);
-    clientHello<<textoCifrado;
-    clientHello.flush();
-    clientHello.close();
-}
 
-void readcifradoServer()
-{
-    Cesar cesarin (masterKey);
-    string leerServer;
-    ifstream serverHello;
-    serverHello.open("documento.txt");
-    serverHello>>leerServer;
-    serverHello.close();
-
-    /*Descifrando a Server*/
-    cesarin.decipher(leerServer);
-
-}
+#endif
--- BOBServer.h
+++ BOBServer.h
@@ -1,7 +1,10 @@
+#ifndef _BOBServer
+#define _BOBServer
+
 #include <iostream>
 #include <fstream>
 #include "diffieHellman.h"
-#include "cesar.h"
+
 using namespace std;
 
 ZZ masterKey;
@@ -46,29 +49,5 @@ void intercambioClaves()
 }
 
 
-void writecifradoServer()
-{
-    /*A partir de este punto todo será cifrado*/
-    Cesar cesarin (masterKey);
-    string textoCifrado = cesarin.cipher("A partir de este punto todo seá cifrado. Atte: Bob");
-    ofstream clientHello;
-    clientHello.open("documento.txt" , std::ofstream::trunc);
-    clientHello<<textoCifrado;
-    clientHello.flush();
-    clientHello.close();
-}
 
-void readcifradoServer()
-{
-    Cesar cesarin (masterKey);
-    string leerServer;
-    ifstream serverHello;
-    serverHello.open("documento.txt");
-    serverHello>>leerServer;
-    serverHello.close();
-
-    /*Descifrando a Server*/
-    cesarin.decipher(leerServer);
-
-}
-
+#endif
